f922e9df5903b1deeff6c7c9957d36146c5a3967
davidbventura <david.ventura@upr.edu>
2025-09-14 03:37:00 +0000

    no he terminado

--- main.cpp
+++ main.cpp
@@ -39,33 +39,75 @@ int main() {
    int menor = randomnum3;
 
     // Hacer codigo para que salgan en orden descendiente
-    if ( mayor > randomnum2) {  // Ver si num1 es mayor que el num2 
-        mayor = randomnum1      // Si num1 es mayor, se guarda en la variable mayor
-        
-        else (mayor < randomnum2)  // Ver si num2 es mayor que el num1
-        mayor = randomnum2  }   // Si num2 es mayor, se guarda en la variable mayor
-    
-    
-        // Este bloque es para verificar cual es mas grande entre el num1 y num2 para ver si es mayor. 
-    
-   /* else if ( mayor > randomnum3) { // Ver si num1 es mayor que el num3 
+    //Primero buscamos cual es el numero mayor
+
+//Verificar si num 1 es mayor que num 2
+    if ( mayor > randomnum2 )   // Ver si num1 es mayor que el num2 
+       {
         mayor = randomnum1;      // Si num1 es mayor, se guarda en la variable mayor
-        else (mayor < randomnum3)  // Ver si num3 es mayor que el num1
+        medio = randomnum2;      // Num 2 se guarda en la variable medio
+        menor = randomnum3;
+       
+        if ( mayor < randomnum2 )  // Ver si num2 es mayor que el num1
+       
+        mayor = randomnum2;    // Si num2 es mayor, se guarda en la variable mayor
+        medio = randomnum1;    // Num1 se guarda en variable medio
+        menor = randomnum3;
+    }
+
+//Verificar si num1 es mayor o menor que num3
+
+    else if ( mayor < randomnum3 )   // Ver si num1 es mayor que el num3 
+       {
+        mayor = randomnum3;      // Si num3 es mayor, se guarda en la variable mayor
+       
+       }
+
+// Ya conseguimos el numero mayor, ahora toca ver cual será el medio y menor
+
+ if ( medio < menor)  // Ver si es mayor que el num3 
+       {
+               int tmp = medio;
+               medio = menor;
+               menor = medio;
+    } 
+
+
+       
+       
+
+    // Este bloque es para verificar cual es mas medio y menor. 
+
+
+   /* else if ( medio > randomnum3)  // Ver si es mayor que el num3 
+       {
+        mayor = randomnum1;      // Si num1 es mayor, se guarda en la variable mayor
+       
+       if (mayor <= randomnum3)  // Ver si num3 es mayor que el num1
+        
         mayor = randomnum3;         // Guardar en variable que num3 es mayor pq es mayor que num 1
 
-    }
+    } 
     
-    else if (randomnum2 > randomnum3) {     // Ver si el num2 es mayor que num3
+    else if (randomnum2 > randomnum3)      // Ver si el num2 es mayor que num3
+     
+    {
         medio = randomnum2;                  // Si num2 es mayor que num3 se le asigna a la variable medio
         menor = randomnum3; 
-        else ( randomnum2 < randomnum3 ) 
+    }
+       else if ( randomnum2 < randomnum3 ) 
+    
+       {
         medio = randomnum3;
         menor = randomnum2;
-    } 
+        }
+    */
         
         
-    cout << "Orden descendiente: " << mayor << ", " << medio << ", " << menor << endl; */
-    return 0; } 
+    cout << "Orden descendiente: " << mayor << ", " << medio << ", " << menor << endl; 
+    
+    return 0; 
+} 
 
 
 
